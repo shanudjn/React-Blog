4e651bd05b8b8fecd9d591a919b41f0fed4321b3
shanudjn <shanudjn@gmail.com>
2020-05-18 13:27:13 +0530

    starter code added

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => (
+    <React.Fragment>
+    <h1>Welcome to my blog</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum non hendrerit mauris. Duis fermentum sem quis lorem blandit laoreet. Curabitur a ipsum eu nulla ornare vulputate in et diam. Integer et laoreet lorem, quis fermentum enim. Aliquam posuere rutrum libero nec ornare. Maecenas auctor sem eu neque euismod fermentum. Ut eget nulla vitae erat fringilla condimentum. Praesent semper magna at pellentesque ultricies. Nulla facilisis diam quis commodo placerat. Maecenas pharetra vestibulum ligula, at ultrices dui eleifend non. Aenean aliquet sem quis arcu maximus, tempus sagittis leo luctus. Nullam ultricies tortor id turpis vestibulum placerat. Duis in placerat urna. Sed hendrerit purus non diam porttitor, nec rutrum est congue. Quisque eget purus odio.
+    </p>
+    <p>Maecenas pharetra quam nunc, tincidunt imperdiet arcu varius in. Nulla facilisi. Phasellus eget justo cursus, sodales ipsum et, vestibulum arcu. Aliquam ac quam vitae purus venenatis accumsan. Vivamus tincidunt lacus porttitor, ultrices libero sit amet, interdum urna. Etiam justo est, aliquam sed porta ac, sollicitudin vel nulla. Nunc dapibus vitae lectus non vulputate</p>
+    <p>Fusce vel quam sem. Suspendisse sed libero dolor. Sed odio massa, venenatis ac aliquet a, aliquam vel augue. Vestibulum malesuada convallis nisi euismod aliquam. Nulla eleifend ipsum sed turpis convallis, et tristique libero ultricies. In dignissim sed ligula vel vestibulum. Maecenas a neque ipsum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id ornare risus. Nulla id porta enim, id vulputate felis. Fusce pharetra, orci et rhoncus fermentum, ligula risus consectetur lorem, ut faucibus velit dui blandit felis. Nam aliquam ornare ante, at dapibus ante congue scelerisque.</p>
+    </React.Fragment>
+)
+export default HomePage;
