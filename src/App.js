b3cef17f1455fe87a924d3118000ea64d92d522c
shanudjn <shanudjn@gmail.com>
2020-05-18 13:53:11 +0530

    added the required components and their routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
-
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
+import ArticlePage from './pages/ArticlePage';
+import ArticlesList from './pages/ArticlesList';
+import AboutPage from './pages/AboutPage';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <HomePage/>
-      </div>
+      <Router>
+        <div className="App">
+          <div id="page-body">
+          <Route path="/" component={HomePage} exact/>
+          <Route path="/article" component={ArticlePage} exact/>
+          <Route path="/articles-list" component={ArticlesList} exact/>
+          <Route path="/about" component={AboutPage} exact/>
+          </div>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/pages/ArticlePage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const ArticlePage = () => (
+    <React.Fragment>
+        <h1>This is an article</h1>
+    </React.Fragment>
+)
+export default ArticlePage;
