0e53833adbcf23f90eef985c3d441f9e91f3ad4c
shanudjn <shanudjn@gmail.com>
2020-05-27 11:17:28 +0530

    changes on app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
 import ArticlePage from './pages/ArticlePage';
@@ -15,11 +15,14 @@ class App extends Component {
         <div className="App">
           <NavBar/>
           <div id="page-body">
-          <Route path="/" component={HomePage} exact/>
-          <Route path="/article/:name" component={ArticlePage} exact/>
-          <Route path="/articles-list" component={ArticlesListPage} exact/>
-          <Route path="/about" component={AboutPage} exact/>
-          <Route component={NotFoundpage}/>
+          <Switch>
+            <Route path="/" component={HomePage} exact/>
+            <Route path="/article/:name" component={ArticlePage} exact/>
+            <Route path="/articles-list" component={ArticlesListPage} exact/>
+            <Route path="/about" component={AboutPage} exact/>
+            <Route component={NotFoundpage}/>
+          </Switch>
+
           </div>
         </div>
       </Router>
--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import articleContent from './article-content';
 import ArticlesList from '../components/ArticlesList';
+import NotFoundPage from './NotFoundPage';
 const ArticlePage = ({match}) => {
     //getting the name from url parameters
     const name = match.params.name;       
     const article = articleContent.find(article => article.name===name);    
 
-    if(!article) return <h1>Article Does Not Exist</h1>
+    if(!article) return <NotFoundPage/>
 
     const otherArticles = articleContent.filter(article => article.name !== name);
     return (
